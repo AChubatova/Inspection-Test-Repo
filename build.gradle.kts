53c59975bac3ca3cb00979f6541268373fffd507
AChubatova <64786092+AChubatova@users.noreply.github.com>
2020-07-21 10:24:22 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,12 +6,14 @@
 
 plugins {
     // Apply the Kotlin JVM plugin to add support for Kotlin.
-    //id("org.jetbrains.kotlin.jvm") version "1.3.72"
-    id("org.jetbrains.kotlin.jvm") version "1.8"
+    id("org.jetbrains.kotlin.jvm") version "1.3.72"
+    //id("org.jetbrains.kotlin.jvm") version "1.8"
     
     // Apply the java-library plugin for API and implementation separation.
     `java-library`
 }
+
+sourceCompatibility = "1.8"
 
 repositories {
     // Use jcenter for resolving dependencies.
@@ -24,8 +26,8 @@ dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
 
     // Use the Kotlin JDK 8 standard library.
-    //implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk1.8")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    //implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk1.8")
     
     // Use the Kotlin test library.
     testImplementation("org.jetbrains.kotlin:kotlin-test")
