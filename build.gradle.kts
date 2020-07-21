7c17f3c1f70467fb444039d58d9a07f9c161a37b
AChubatova <64786092+AChubatova@users.noreply.github.com>
2020-07-21 10:18:44 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,8 +24,9 @@ dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
 
     // Use the Kotlin JDK 8 standard library.
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
+    //implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk1.8")
+    
     // Use the Kotlin test library.
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 
